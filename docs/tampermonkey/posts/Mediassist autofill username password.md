2692079b49077728ae55540182b7ed6ba64e8310
Ankit Meel <ankitjmeel@gmail.com>
2025-08-12 15:46:23 +0530

    fix medibuddy mediassist scripts

--- docs/tampermonkey/posts/Mediassist autofill username password.md
+++ docs/tampermonkey/posts/Mediassist autofill username password.md
@@ -2,7 +2,7 @@
 title: Mediassist autofill username password
 date:
   created: 2025-07-15
-  updated: 2025-07-16
+  updated: 2025-08-12
 ---
 
 <!-- GENERATED FILE -->
--- docs/tampermonkey/upd_y/Mediassist autofill username password.user.js
+++ docs/tampermonkey/upd_y/Mediassist autofill username password.user.js
@@ -11,18 +11,16 @@
 // @require      https://gist.githubusercontent.com/adamhotep/7c9068f2196326ab79145ae308b68f9e/raw/373f5e8405b98781001aea9a9e74585367344960/waitForKeyElements.js
 // ==/UserScript==
 
-waitForKeyElements(
-  '[id="username"]',
-  function () {
-    "use strict";
-    console.log("blahfoo");
-    var y = document.querySelectorAll('input[class="form-control"]');
-    y[0].setAttribute("autocomplete", "email");
-    y[0].setAttribute("type", "text");
-    y[1].setAttribute("autocomplete", "current-password");
-    y[1].setAttribute("required", "on");
-    var form = document.querySelectorAll('form[id="MediForm"]')[0];
-    form.setAttribute("autocomplete", "on");
-  },
-  false
-);
+function fix() {
+  "use strict";
+  console.log("blahfoo");
+  const y = document.querySelectorAll('input[class="form-control"]');
+  y[0].setAttribute("autocomplete", "email");
+  y[0].setAttribute("type", "text");
+  y[1].setAttribute("autocomplete", "current-password");
+  y[1].setAttribute("required", "on");
+  const form = document.querySelectorAll('form[id="MediForm"]')[0];
+  form.setAttribute("autocomplete", "on");
+}
+
+waitForKeyElements('[id="username"]', fix, false);
