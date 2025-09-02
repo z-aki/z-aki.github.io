b265abbe4b6a36a9166ad4d22ef32d6a6474bb03
Ankit Meel <ankitjmeel@gmail.com>
2025-09-02 18:35:08 +0530

    tamper/indian bank url parameter

---
+++ docs/tampermonkey/posts/Indian bank CGRS autofill complaint from URL.md
@@ -0,0 +1,21 @@
+---
+title: Indian bank CGRS autofill complaint from URL
+date:
+  created: 2025-09-02
+  updated: 2025-09-02
+---
+
+<!-- GENERATED FILE -->
+[Install latest with possible future updates](../upd_y/Indian bank CGRS autofill complaint from URL.user.js)
+OR
+[Install fixed script with no updates](../upd_n/Indian bank CGRS autofill complaint from URL.user.js)
+```js show_lines="1:10"
+---8<--- "docs/tampermonkey/upd_n/Indian bank CGRS autofill complaint from URL.user.js::100"
+```
+<!-- more -->
+---8<--- "docs/tampermonkey/warning.txt"
+```js
+---8<--- "docs/tampermonkey/upd_n/Indian bank CGRS autofill complaint from URL.user.js:1:"
+```
+
+------------
---
+++ docs/tampermonkey/upd_y/Indian bank CGRS autofill complaint from URL.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Indian bank CGRS autofill complaint from URL
+// @version      0.1
+// @description  Autofills the complaint number using the OPT parameter in the URL.
+// @author       https://github.com/z-aki
+// @namespace    https://github.com/z-aki
+// @match        https://apps.indianbank.in/cgrc*
+// @icon         https://apps.indianbank.in/cgrc/assets/images/logo.png
+// @grant        none
+// @require      https://github.com/adamhotep/nofus.js/raw/refs/heads/main/nofus.js
+// @require      https://cdn.jsdelivr.net/npm/crypto-js@4.2.0/crypto-js.min.js
+// ==/UserScript==
+
+function fix(elem) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const opt = urlParams.get("opt");
+  if (opt) {
+    setTimeout(() => {
+      elem.value = opt.replace("track", "");
+    }, 1000);
+  }
+}
+
+nf.wait$("input#Txt_ticket_no", fix);
