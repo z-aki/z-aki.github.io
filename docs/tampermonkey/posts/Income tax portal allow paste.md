b7ded95d2893da4139fb2cc45cd2ee9389455476
Ankit Meel <ankitjmeel@gmail.com>
2025-09-04 03:27:57 +0530

    tamper/income tax allow paste

---
+++ docs/tampermonkey/posts/Income tax portal allow paste.md
@@ -0,0 +1,21 @@
+---
+title: Income tax portal allow paste
+date:
+  created: 2025-09-04
+  updated: 2025-09-04
+---
+
+<!-- GENERATED FILE -->
+[Install latest with possible future updates](../upd_y/Income tax portal allow paste.user.js)
+OR
+[Install fixed script with no updates](../upd_n/Income tax portal allow paste.user.js)
+```js show_lines="1:10"
+---8<--- "docs/tampermonkey/upd_n/Income tax portal allow paste.user.js::100"
+```
+<!-- more -->
+---8<--- "docs/tampermonkey/warning.txt"
+```js
+---8<--- "docs/tampermonkey/upd_n/Income tax portal allow paste.user.js:1:"
+```
+
+------------
---
+++ docs/tampermonkey/upd_n/Income tax portal allow paste.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Income tax portal allow paste
+// @version      0.1
+// @description  Allow paste on income tax eportal IEC
+// @author       https://github.com/z-aki
+// @namespace    https://github.com/z-aki
+// @match        https://eportal.incometax.gov.in/*
+// @icon         https://external-content.duckduckgo.com/ip3/www.incometax.gov.in.ico
+// @grant        none
+// @require      https://github.com/adamhotep/nofus.js/raw/refs/heads/main/nofus.js
+// @downloadURL  none
+// ==/UserScript==
+
+function fix(elem) {
+  "use strict";
+  console.log("blahfoo allow paste");
+  elem.removeAttribute("onpaste");
+  elem.setAttribute("autocomplete", "on");
+}
+
+nf.wait$("input[onpaste]", fix);
