d1f6af5303ec9161a8a21e385644ed5b9b7590ee
Ankit Meel <ankitjmeel@gmail.com>
2025-12-24 19:32:31 +0530

    tamper/ipgrs: scrolling grievance

--- docs/tampermonkey/upd_y/IPGRS karnataka remove waiting GIF.user.js
+++ docs/tampermonkey/upd_y/IPGRS karnataka remove waiting GIF.user.js
@@ -16,4 +16,11 @@ function fix(elem) {
   elem.remove();
 }
 
+function fixChild(elem){
+    const className = elem.classList[0];
+    const styl = `.${className} { max-height: 1000px !important; }`;
+    nf.style$(styl, elem);
+}
+
+nf.wait$("div.dataTables_scrollBody", fixChild);
 nf.wait$("div#preloder", fix);
